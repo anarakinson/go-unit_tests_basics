53c5b496e068cdd4a4b79d615224f20b23475e9d
anarakinson <anarakinson@gmail.com>
2022-11-26 16:59:15 +0300

    add test table

--- testing/max_test.go
+++ testing/max_test.go
@@ -8,21 +8,48 @@ import (
 
 func TestMax(t *testing.T) {
     // arrange
-    numbers := []int{0, -1, 1, 3, -5}
-    expected := 3
+    testTable := []struct{
+        numbers []int
+        expected int
+    }{
+        {
+            numbers : []int{0, -1, 1, 3, -5},
+            expected : 3,
+        },
+        {
+            numbers : []int{0, -1, -10, -5},
+            expected : 0,
+        },
+        {
+            numbers : []int{},
+            expected : 0,
+        },
+    }
 
     // act
-    result := utils.Max(numbers)
+    for _, testCase := range(testTable) {
+        result := utils.Max(testCase.numbers)
+        t.Logf(
+            "Calling Max(%v)\n expected: %d\n result: %d\n\n",
+            testCase.numbers,
+            testCase.expected,
+            result,
+        )
 
-    //assert
-    if result != expected {
-        t.Errorf("Incorrect result. Expected %d, got %d", expected, result)
+        //assert
+        if result != testCase.expected {
+            t.Errorf(
+                "Incorrect result. Expected %d, got %d",
+                testCase.expected,
+                result,
+            )
+        }
     }
 }
 
 func TestMaxIdx(t *testing.T) {
     // arrange
-    numbers := []int{0, -1, 3, -5}
+    numbers := []int{0, -1, 3, 1, -5}
     expected := 2
 
     // act
